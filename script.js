c2b99abc1cadaa51118a515b4909c912cda6547d
ketarubot <robin09v@naver.com>
2025-07-11 13:29:21 +0900

    5

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let performance = [0, 0, 0];
 let randomMode = false;
 let english = false;
 let running = false;
+let fileRead = false;
+const lsgi = localStorage.getItem.bind(localStorage);
 
 function readFile(file) {
   const reader = new FileReader();
@@ -17,7 +19,10 @@ function readFile(file) {
       wordDict[word] = meaning;
       weights[word] = 1;
     }
-    chooseQuestion();
+    localStorage.setItem('wordDict', JSON.stringify(wordDict));
+    localStorage.setItem('weights', JSON.stringify(weights));
+    setWordList();
+    fileRead = true;
   }
   reader.readAsText(file);
 }
@@ -28,6 +33,7 @@ function reset() {
   correct = new Set();
   performance = [0, 0, 0];
   randomMode = false;
+  fileRead = false;
 }
 
 async function chooseQuestion() {
@@ -41,10 +47,37 @@ async function chooseQuestion() {
   }
   running = true;
   while (running) {
-    await askQuestion();
+    await separateQuestion();
   }
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function extract(words, type, exclude, count) {
+  let iter;
+  if (type === 'k') {
+    iter = Object.values(words);
+  } else if (type === 'e') {
+    iter = Object.keys(words);
+  }
+  
+  shuffle(iter);
+  let result = [];
+  let i = 0;
+  for (const word of iter) {
+    if (word !== exclude) {
+      result.push(word);
+      i += 1;
+    }
+    if (i >= count) break;
+  }
+  return result;
+}
 
 function cleanInput(inputStr = '') {
     return inputStr.replace(/\(.*?\)|~/g, '').replace(/ /g, '').replace(/'/g, '’').trim().toLowerCase();
@@ -60,7 +93,7 @@ function randomChoice(arr) {
   return arr[randomIndex];
 }
 
-async function askQuestion() {
+async function separateQuestion() {
   let remainingWords = [];
   for (let word of Object.keys(wordDict)) {
     if (!correct.has(word)) {
@@ -77,12 +110,8 @@ async function askQuestion() {
   if (weightedChoices.length === 0) {
     alert('모든 단어를 학습하셨습니다.');
     running = false;
-    if (prompt('다시 학습하시겠습니까(y/*)? ') === 'y') {
-      main();
-      return;
-    } else {
-      return; 
-    }
+    testStart.style.display = 'block';
+    return;
   }
 
   const questionWord = randomChoice(weightedChoices);
@@ -104,6 +133,10 @@ async function askQuestion() {
     correctAnswer = answer;
   }
 
+  await askQuestion(question, correctAnswer, remainingWords, questionWord);
+}
+
+async function askQuestion(question, correctAnswer, rws, qw) {
   const displayQuestion = document.getElementById('question');
   displayQuestion.innerText = `'${question}': `;
   const userInput = document.getElementById('userInput');
@@ -131,39 +164,97 @@ async function askQuestion() {
     }, 100);
   });
   }
-  const check = await waitForAnswer();
+  await waitForAnswer();
   performance[1]++;
 
+  printResult(userAnswer, correctAnswer, rws, qw);
+}
 
+function printResult(userAnswer, correctAnswer, rws, qw) {
   const printResult = document.getElementById('printResult');
   if (isCorrect(userAnswer, correctAnswer)) {
-    correct.add(questionWord);
+    correct.add(qw);
     performance[0]++;
     performance[2]++;
-    weights[questionWord] = Math.max(1, weights[questionWord] - 1);
+    weights[qw] = Math.max(1, weights[qw] - 1);
     printResult.innerText = `정답입니다!: '${correctAnswer}'`;
   } else {
     performance[0] = 0;
-    weights[questionWord]++;
-    printResult.innerText = `틀렸습니다. 정답은 '${correctAnswer}'입니다.`;
+    weights[qw]++;
+    printResult.innerText = `틀렸습니다. 정답은 '${qw}'입니다.`;
   }
   const accuracy = ((performance[2] / performance[1]) * 100).toFixed(2);
   printResult.innerHTML += `<br>정답률: ${accuracy}%`;
   printResult.innerHTML += `<br>연속 정답 수: ${performance[0]}`;
-  printResult.innerHTML += `<br>남은 문제 수: ${remainingWords.length-1}`;
+  printResult.innerHTML += `<br>남은 문제 수: ${rws.length-1}`;
 }
 
-function main(file) {
-  reset();
-  readFile(file);
+function changeAnswerInput() {
+  const answerCategory = parseInt(document.getElementById('answerInput').value);
+  const userInput = document.getElementById('userInput');
+  const userSelect = document.getElementById('userSelect');
+  if (answerCategory === 1) {
+    userInput.setAttribute('style', 'display: block');
+    userSelect.setAttribute('style', 'display: none');
+  } else if (answerCategory === 2) {
+    userInput.setAttribute('style', 'display: none');
+    userSelect.setAttribute('style', 'display: block');
+  }
 }
 
 const fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', (e) => {main(e.target.files[0])});
+fileInput.addEventListener('change', (e) => {
+  reset();
+  readFile(e.target.files[0]);
+});
+
+const sideMenu = document.getElementById('sideMenu');
 
 function toggleMenu() {
-  document.getElementById('sideMenu').classList.toggle('open');
+  sideMenu.classList.toggle('open');
   document.getElementById('menuBackground').classList.toggle('open');
+  if (sideMenu.classList.contains('open')) {
+    setWordList();
+    document.getElementById('delete').addEventListener('click', () => {
+      localStorage.clear()
+      const wordList = document.getElementById('wordList');
+      while (wordList.firstChild) {
+        wordList.removeChild(wordList.firstChild);
+      }
+    });
+  }
 }
+
+const testStart = document.getElementById('testStart');
+testStart.addEventListener('click', () => {
+  if (lsgi('wordDict') && lsgi('weights')) {
+    wordDict = JSON.parse(lsgi('wordDict'));
+    weights = JSON.parse(lsgi('weights'));
+    fileRead = true;
+  }
+  if (fileRead === true) {
+    testStart.style.display = 'none';
+    chooseQuestion();
+  } else {
+    alert('읽힌 파일이 없습니다!');
+  }
+});
+
+function setWordList() {
+  if (lsgi('wordDict')) {
+    const words = JSON.parse(lsgi('wordDict'));
+    const wordList = document.getElementById('wordList');
+    while (wordList.firstChild) {
+      wordList.removeChild(wordList.firstChild);
+    }
+    for (const [word, meaning] of Object.entries(words)) {
+      const add = document.createElement('li');
+      add.innerText = `${word}: ${meaning}`;
+      wordList.appendChild(add);
+    }
+  }
+}
+
+changeAnswerInput();
 
 // responsiveVoice.speak('hi');
